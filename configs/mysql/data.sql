9a2cfffeff50507aea40d21bf3099984a64ffeab
Franklim Paulino <franklim.ti@gmail.com>
2024-04-04 09:27:06 -0300

    evo 3

--- configs/mysql/data.sql
+++ configs/mysql/data.sql
@@ -5,6 +5,7 @@ create table regismoto_db.registros
         primary key,
     nome           varchar(200) not null,
     telefone       varchar(20)  not null,
+    cpf            varchar(20)  not null,
     endereco       varchar(200) null,
     email          varchar(200) not null,
     placa_moto     varchar(200) null,
